a01f13a0ad909bb0d9a8f45eadd1b8d7e043916e
santiadlv <49079265+santiadlv@users.noreply.github.com>
2021-11-30 18:44:28 -0700

    Initial commit

---
+++ api/core/config.py
@@ -0,0 +1,16 @@
+from pydantic import BaseSettings
+from secrets import token_urlsafe
+
+class Settings(BaseSettings):
+    SECRET_KEY: str = token_urlsafe(32)
+    PROJECT_NAME: str
+    ADMIN_EMAIL: str
+    MONGODB_URL: str
+    MONGODB_NAME: str
+    MONGODB_COLLECTION_PLACES: str
+
+    class Config:
+        env_file = '.env'
+        env_file_encoding = 'utf-8'
+
+settings = Settings()
